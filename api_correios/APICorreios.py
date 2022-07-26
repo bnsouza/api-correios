d27c44d15479027d96618403e2c361f05dbb2976
Christopher Frige <chrisfrige@gmail.com>
2022-07-26 17:55:58 -0300

    Criar prepostagem e ajuste nos campos obrigatórios

--- api_correios/APICorreios.py
+++ api_correios/APICorreios.py
@@ -1,7 +1,7 @@
 from typing import Any, List, Optional, Union
 import json
 import requests
-from schema.correios import Ambiente, PrepostagemConsulta, Prepostagens
+from schema.correios import Ambiente, ListarPrepostagensRequest, Prepostagens, Prepostagem
 from utils.token import token_is_expired
 
 class APICorreios:
@@ -21,7 +21,7 @@ class APICorreios:
         self.base_url = "https://apihom.correios.com.br" if self.ambiente=='homologacao' else "https://api.correios.com.br"
         self.token = None
 
-    def autenticar(self):
+    def autentica(self):
         token_expired = False
         if self.token:
             token_expired = token_is_expired(self.token)
@@ -46,13 +46,9 @@ class APICorreios:
         return True
 
     def listar_prepostagens(self, **kwargs: dict) -> Prepostagens:
-        self.autenticar()
+        self.autentica()
 
-        if not 'numeroCartaoPostagem' in kwargs:
-            kwargs.update({'numeroCartaoPostagem': self.cartao_postagem})
-
-        params = PrepostagemConsulta(**kwargs)
-
+        params = ListarPrepostagensRequest(**kwargs)
         headers = {'Authorization': 'Bearer ' + self.token}
 
         response = requests.get(
@@ -60,14 +56,74 @@ class APICorreios:
             params=params, 
             headers=headers
         )  
-
-        print(response.json())
-
         response = Prepostagens(**response.json())
+        response = response.dict(exclude_none=True)
+
+        print(response)
         
         return response
 
+    def criar_prepostagem(self, **kwargs: dict) -> Prepostagem:
+        self.autentica()
+
+        payload = Prepostagem(**kwargs)
+        headers = {
+            'Authorization': 'Bearer ' + self.token,
+            "Content-Type": "application/json"
+        }
+
+        response = requests.post(
+            url=self.base_url + '/prepostagem/v1/prepostagens', 
+            data=payload.json(exclude_none=True), 
+            headers=headers
+        )  
+        response = Prepostagem(**response.json())
+        response = response.dict(exclude_none=True)
+        
+        return response
+
+
+
 if __name__ == '__main__':
     correios = APICorreios()
-    correios.listar_prepostagens(tipoObjeto='TODOS', page=0, size=50)
+    # correios.listar_prepostagens(page=0, size=50, tipoObjeto= 'TODOS')
+    # correios.criar_prepostagem(codigoServico= "03220",
+    #     destinatario= {
+    #         'nome': "commodo dolor veniam minim Lorem",
+    #         'dddCelular': "11",
+    #         'celular': "987875847",
+    #         'email': "teste2312@gmail.com",
+    #         'cpfCnpj': "38809594045",
+    #         'obs': "consequat quis fugiat",
+    #         'endereco': {
+    #         'cep': "12234005",
+    #         'logradouro': "dolore dolore",
+    #         'numero': "132",
+    #         'complemento': "in id",
+    #         'bairro': "et culpa",
+    #         'cidade': "incididunt laborum",
+    #         'uf': "SP"
+    #         }
+    #     },
+    #     remetente= {
+    #         'nome': "et labore",
+    #         'dddCelular': "12",
+    #         'celular': "987478574",
+    #         'email': "teste@gmail.com",
+    #         'cpfCnpj': "64907455003",
+    #         'obs': "esse in reprehenderit Ut",
+    #         'endereco': {
+    #         'cep': "12234005",
+    #         'logradouro': "veniam mollit Ut dolor",
+    #         'numero': "223",
+    #         'complemento': "deserunt sed ipsum nisi",
+    #         'bairro': "sit veniam",
+    #         'cidade': "cupidatat ad non",
+    #         'uf': "SP"
+    #         }
+    #     },
+    #     codigoFormatoObjetoInformado= "1",
+    #     pesoInformado= "10",
+    #     cienteObjetoNaoProibido= 1
+    # )
 
